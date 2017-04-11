1d4393de55d50cb7e6dd29b05f4b0b5201b8a000
Hongbo He <hhbcarl@gmail.com>
2017-04-11 22:18:06 +0800

    Blog style ok

--- iamhhb/http/blog.py
+++ iamhhb/http/blog.py
@@ -6,3 +6,8 @@ bp = Blueprint('blogs', __name__)
 @bp.route('')
 def index():
     return render_template('blogs/index.html.j2')
+
+
+@bp.route('/<slug>')
+def item(slug):
+    return render_template('blogs/show.html.j2')
