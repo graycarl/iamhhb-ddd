b0056e0a133905ae8651b1792809d0712b5f9247
Hongbo He <hhbcarl@gmail.com>
2017-04-12 21:25:33 +0800

    Add reading index

--- iamhhb/http/reading.py
+++ iamhhb/http/reading.py
@@ -7,3 +7,8 @@ bp = Blueprint('reading', __name__)
 @bp.route('/')
 def index():
     return render_template('reading/index.html.j2')
+
+
+@bp.route('/<isbn>')
+def item(isbn):
+    return render_template('reading/item.html.j2')
