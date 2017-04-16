d9c0b366dd26e95f515ebbf523c2a033cac47cc2
Hongbo He <hhbcarl@gmail.com>
2017-04-16 11:18:18 +0800

    Add edit blog page

--- iamhhb/http/blog.py
+++ iamhhb/http/blog.py
@@ -8,6 +8,16 @@ def index():
     return render_template('blogs/index.html.j2')
 
 
+@bp.route('/new')
+def new():
+    return render_template('blogs/new.html.j2')
+
+
 @bp.route('/<slug>')
 def item(slug):
     return render_template('blogs/show.html.j2')
+
+
+@bp.route('/<slug>/edit')
+def edit(slug):
+    return render_template('blogs/edit.html.j2')
