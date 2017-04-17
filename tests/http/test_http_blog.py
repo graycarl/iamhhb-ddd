ed112ae7ad084c5bb62d6983fc74d4267c0c0044
Hongbo He <hhbcarl@gmail.com>
2017-04-17 22:42:01 +0800

    Add test

--- tests/http/test_http_blog.py
+++ tests/http/test_http_blog.py
@@ -1,3 +1,12 @@
+import pytest
+from iamhhb.models import BlogRepository, Blog
+
+
+@pytest.fixture
+def blog_repo(db):
+    return BlogRepository(db)
+
+
 def test_add_blog(app):
     assert '创建文章'.encode('utf-8') in app.get('/blogs/new').data
 
@@ -8,3 +17,23 @@ def test_add_blog(app):
         content='ajajajajja'
     ))
     assert resp.status_code == 302
+
+
+def test_edit_blog(app, blog_repo, db):
+    b1 = Blog(
+        slogan='test-article-1',
+        title='TTTT',
+        sub_title='这是一个副标题',
+        content='这里是文章的内容'
+    )
+    blog_repo.save(b1)
+    db.commit()
+    assert blog_repo.get(b1.id).title == 'TTTT'
+
+    resp = app.post('/blogs/%s/edit' % b1.id, data=dict(
+        title='AAAA',
+        sub_title='这是一个副标题',
+        content='这里是文章的内容'
+    ))
+    assert resp.status_code == 302
+    assert blog_repo.get(b1.id).title == 'AAAA'
